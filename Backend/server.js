e9791853753f7605e037c252d12b8ada5e6b3b75
cristian1s <crisspy15k@gmail.com>
2024-08-03 13:45:16 -0500

    s

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const courseRoutes = require('./routes/courseRoutes');
-const userRoutes = require('./routes/userRoutes');
-const paymentRoutes = require('./routes/paymentRoutes');
-const orderRoutes = require('./routes/orderRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const courseRoutes = require("./routes/courseRoutes");
+const userRoutes = require("./routes/userRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 dotenv.config();
 
@@ -16,17 +16,25 @@ connectDB();
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  origin: "http://localhost:5173",
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 
-app.use('/api', courseRoutes);
-app.use('/api', userRoutes);
-app.use('/api', paymentRoutes);
-app.use('/api', orderRoutes);
+app.use("/api", courseRoutes);
+app.use("/api", userRoutes);
+app.use("/api", paymentRoutes);
+app.use("/api", orderRoutes);
 
+fetch("https://api.ipify.org?format=json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Client IP address:", data.ip);
+  })
+  .catch((error) => {
+    console.error("Error fetching IP address:", error);
+  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
